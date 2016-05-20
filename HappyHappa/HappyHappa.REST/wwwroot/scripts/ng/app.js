6c8389b83babaf664958c65db8d4c6dc9d64a1a4
Christian Siber <siber.christian@gmail.com>
2016-05-20 23:55:04 +0200

    wwwroot initialized with angular and bootstrap

---
+++ HappyHappa/HappyHappa.REST/wwwroot/scripts/ng/app.js
@@ -0,0 +1,28 @@
+﻿var app = angular.module("happyhappa", ["ui.router"]);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/home');
+
+  $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: 'scripts/ng/home/home.html'
+      })
+      .state('recipes', {
+        url: '/recipes',
+        templateUrl: 'scripts/ng/recipes/recipes.html'
+      })
+    .state('intellicook', {
+      url: '/intellicook',
+      templateUrl: 'scripts/ng/intellicook/intellicook.html'
+    })
+      .state('fridge', {
+        url: '/fridge',
+        templateUrl: 'scripts/ng/fridge/fridge.html'
+      })
+      .state('settings', {
+        url: '/settings',
+        templateUrl: 'scripts/ng/settings/settings.html'
+      })
+
+});
