d02aa905c183943145a00d0f0bfdf3d7767b8ccc
Markus Dietrich <mdi1984@gmail.com>
2016-05-20 23:35:46 +0200

    basic rest client and put item stuff

---
+++ HappyHappa/HappyHappa.RestClient/RestManagerBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace HappyHappa.RestClient
+{
+  public class RestManagerBase
+  {
+    public async Task<object> PostWithJsonPayload(string url, object payload)
+    {
+      using (var client = new HttpClient())
+      {
+        var jsonPayload = JsonConvert.SerializeObject(payload);
+        var responseMessage = await client.PutAsync(
+         url,
+         new StringContent(
+             jsonPayload.ToString(),
+             Encoding.UTF8,
+             "application/json"));
+
+        return responseMessage;
+      }
+    }
+  }
+}
